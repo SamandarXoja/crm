03a26ca26770276840d8b01d1272e520acd9af24
User <samandarsaidahmadov98@gmail.com>
2025-03-05 17:29:21 +0500

    real estate

--- src/pages/real-estate/index.tsx
+++ src/pages/real-estate/index.tsx
@@ -1,8 +1,48 @@
 function RealEstate() {
     return (
-        <div>
-            RealEstate
-        </div>
+        <section className="px-5">
+            <div className="flex justify-around">
+                <div className="cards mt-5">
+                    <h3 className="text-center mb-2">Lorem, ipsum dolor111.</h3>
+                 
+                    <div className="flex flex-col gap-4 mt-10">
+                        <div className="bg-white border shadow-md w-[300px] min-h-[200px] p-2">
+                            <h3>Lorem, ipsum.</h3>
+                            <p>Lorem, ipsum dolor.</p>
+                        </div>
+
+                    </div>
+
+                </div>
+                <div className="cards mt-5">
+                    <h3 className="text-center mb-2">Lorem, ipsum dolor.</h3>
+            
+                    <div className="flex flex-col gap-4 mt-10">
+                        <div className="bg-white border shadow-md w-[300px]  min-h-[200px] p-2">
+                            <h3>Lorem, ipsum.</h3>
+                            <p>Lorem, ipsum dolor.</p>
+                        </div>
+
+                    </div>
+
+                </div>
+                <div className="cards mt-5">
+                    <h3 className="text-center mb-2">Lorem, ipsum dolor.</h3>
+                
+                    <div className="flex flex-col gap-4 mt-10">
+                        <div className="bg-white border shadow-md w-[300px] min-h-[200px] p-2">
+                            <h3>Lorem, ipsum.</h3>
+                            <p>Lorem, ipsum dolor.</p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+
+            </div>
+        </section>
     )
 }
 
